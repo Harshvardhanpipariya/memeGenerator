1be16d656a927d54923b845b0cc6789203ffbc40
harshvardhanpipariya <harshvardhanpipariya65@gmail.com>
2024-06-08 23:37:09 +0530

    remove h2 meme title

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
             loadingSpinner.classList.add('hidden');
             memeContainer.innerHTML = `
-                <h2>${meme.title}</h2>
                 <img src="${meme.url}" alt="${meme.author}">
             `;
         })
